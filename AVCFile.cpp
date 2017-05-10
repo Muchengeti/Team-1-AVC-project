3fca0969bf07a1d33ffc5978d3afbba1a2654a55
Devon Gregory <gregordevo@mulberry-garden.ecs.vuw.ac.nz>
2017-05-10 15:52:43 +1200

    second commit

--- AVCFile.cpp
+++ AVCFile.cpp
@@ -3,9 +3,11 @@
 #include "E101.h"
 
 int checkPosition(int array[]);
-int move(int direction);
+int move(int Error);
 int read_ir();
 int reverse();
+int degree90(int Error);
+bool third_quadrant = false;
 
 int main(){ 
 	
@@ -37,8 +39,17 @@ int main(){
 	Parse = checkPosition(PixelColours);
 	if(Parse == 999){
 		reverse();
-	}else{
-	move(Parse);
+	}else if (Parse == 666 && third_quadrant == false)
+	{
+		third_quadrant =true;
+	}
+	}else if(third_quadrant == false){
+		move(Parse);
+	}else if(Parse == 666){
+		degree90(Parse);
+	}
+	else{
+		degree90(Parse);
 	}
 }	
 	
@@ -48,47 +59,51 @@ int main(){
 int checkPosition(int array[]){
 	int sum=0;
 	int count=0;
+	int countwhite=0;
 	for(int i=0; i < 32; i++){
 		
 		if(array[i] == 1){
 			sum = sum + (i-16.5)*5;
+			countwhite++;
 		}
 		else{
 			count++;
 		}
+		
 	}
 
 	if (count == 32){
 		return 999;
-	}else {
-	return sum;
-}
+	}else if (countwhite == 32){
+		return 666;
+	}
+	else {
+		return sum;
+	}
 }
 int move(int Error){
-	int motor_speed;
-	if (Error > 0){
+	int motor_speed =0; 
+	printf("Error = %d\n ", Error); 
+	
+	if(Error > 0) {
 		motor_speed =(int)((double)Error/800 *50 + 150);
+		set_motor(2,200);
+		set_motor(1, (int)(motor_speed));
+		printf("Motor Speed = %d\n ", motor_speed);
+	}else if(Error < 0) {
+		motor_speed = -(int)((double)Error/800 *50 - 150);
+		set_motor(2,(int)motor_speed);
+		set_motor(1,200);
+		printf("Motor Speed = %d\n ", motor_speed);
 	}
-	else if (Error< 0){
-			motor_speed = -(int)((double)Error/800 *50 - 150);
-	}
+
 	else{
 		motor_speed = 180;
-	}
-	printf("Motor Speed = %d\n ", motor_speed);
-	printf("Error = %d\n ", Error);
-	if(Error > 0) {
-		set_motor(2,200);
-		 set_motor(1, (int)(motor_speed));
-	 
-	}else if(Error < 0) {
-		set_motor(2,(int)motor_speed);
-	 set_motor(1,200);
-	}
-	else{
-	set_motor(2,(int)(motor_speed));
-    set_motor(1, (int)(motor_speed));
-}
+		set_motor(2,(int)(motor_speed));
+		set_motor(1, (int)(motor_speed));
+		printf("Motor Speed = %d\n ", motor_speed);
+	} 
+	
 	return 0;
 }
 int reverse(){
@@ -98,3 +113,29 @@ int reverse(){
 	printf("\n reverse");
 	return 0;
 }
+int degree90(int Error) {
+	int motor_speed;
+	if(Error > 0) {
+		motor_speed =(int)((double)Error/800 *50 + 150);
+		set_motor(2,200);
+		set_motor(1, (int)(motor_speed));
+		printf("Motor Speed = %d\n ", motor_speed);
+	}else if(Error < 0) {
+		motor_speed = -(int)((double)Error/800 *50 - 150);
+		set_motor(2,(int)motor_speed);
+		set_motor(1,200);
+		printf("Motor Speed = %d\n ", motor_speed);
+	}else if (Error == 666) {
+		set_motor(2,120);
+		set_motor(1, 180);
+	}
+
+	else{
+		motor_speed = 180;
+		set_motor(2,(int)(motor_speed));
+		set_motor(1, (int)(motor_speed));
+		printf("Motor Speed = %d\n ", motor_speed);
+	} 
+	return 0;
+}
+
